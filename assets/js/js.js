e96b1fbee3b0ce7a98fb5cd01b44aa60e2abd612
Bo3lii <aoosa.alhaddad1@gmail.com>
2022-04-03 00:54:41 +0300

    a

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,6 @@
+
+// sidebar
+
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 
@@ -5,3 +8,28 @@ btn.onclick = function(){
     sidebar.classList.toggle("active");
     
 }
+
+
+
+// Sign In page 
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const loginForm = document.querySelector("#login");
+const createAccountForm = document.querySelector("#createAccount");
+
+document.querySelector("#linkCreateAccount").addEventListener("click", e =>{
+
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
+    });
+
+document.querySelector("#linkLogin").addEventListener("click", e =>{
+
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+            });
+
+});
